673fbd647187d0c8cb7a16648dc6009813496a05
yafoo123 <yafettekie4@gmail.com>
2025-09-05 17:35:01 +0200

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MyClass {
+   public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n;
+        while (true){
+            System.out.print("Skriv inn et heltall: ");
+            n = in.nextInt();
+            if(n > 0){
+                break;
+            } else{
+                System.out.println("Ugyldig heltall");
+            }
+        }
+        long fakultet = 1;
+        for(int i = 1; i <= n; i++){
+            fakultet *= i;
+        }
+        System.out.println(n + "!= " + fakultet);
+
+        in.close();
+    }
+}
